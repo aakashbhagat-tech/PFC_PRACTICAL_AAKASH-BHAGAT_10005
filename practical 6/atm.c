dd82e942f3f2d2c3840a3984e71602473e08732f
aakashbhagat-tech <aakash.bhagat@rungta.org>
2025-12-26 20:32:50 +0530

    added practical 6

--- practical 6/atm.c
+++ practical 6/atm.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
-int main(){
-  //name 
-  //erp
+int main(){//Aakash bhagat //ERP - 10005
+  int pin,originalpin = 1234;
+
+  printf("enter ATM PIN: ");
+  scanf("%d",&pin);
+  if(pin==originalpin)
+  printf("Access granted");
+  else
+  printf("wrong PIN");
   return 0;
 }
+
+//o/p :
+//
